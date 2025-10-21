30693eb58a0e42698da0bc292a596d7822284755
khanhlee <khanhdev2k@gmail.com>
2025-10-21 16:52:40 +0700

    basic dropdown list

--- tests/udemy-practice/uiComponent.spec.ts
+++ tests/udemy-practice/uiComponent.spec.ts
@@ -20,4 +20,44 @@ test.describe("Nav to page Forms: ", () => {
         const textEmailInput = emailInput.inputValue();
         expect(textEmailInput).toEqual('khanhdeptraivcl@gmail.com')
     })
+
+    test('Radio button: ', async ({page}) => {
+        const opt1 = page.locator('nb-card', {hasText: 'Using the Grid'}).getByLabel('Option 1');
+        const opt2 = page.locator('nb-card', {hasText: 'Using the Grid'}).getByRole('radio', {name: 'Option 2'});   
+        const optDisabled = page.locator('nb-card', {hasText: 'Using the Grid'}).getByLabel('Disabled Option');
+
+        await opt1.click({force: true});
+        
+        expect (opt1).toBeTruthy();
+        expect(opt2).not.toBeTruthy();
+        expect (optDisabled).toBeDisabled();
+
+    })
 })  
+
+test.describe("Nav to Modal & Overlays", () => {
+    test.beforeEach("Nav to page Forms: ", async ({page} )    => {
+    
+})
+    test('CheckBoxes: ', async ({page}) => {
+        await page.getByText('Modal & Overlays').click();
+        await page.getByText('Toastr').click();
+        await page.getByText('Hide on click').uncheck({force: true});
+        await page.getByText('Prevent arising of duplicate toast').check();
+
+        // const allBoxes = page.getByRole('checkbox');
+        // for( const box of await allBoxes.all({force: true})){
+        //     await box.check();
+        //     expect(await box.isChecked()).toBeTruthy();
+        // }
+    })
+
+    test('Drop down list: ', async ({page}) => {
+        const dropdown = page.locator('ngx-header nb-select');
+        await dropdown.getByRole('button').click();
+        await page.waitForTimeout(2000);
+
+        const listItem = page.locator('nb-option-list nb-option');
+        await listItem.getByText('Dark').click();
+    })
+})
